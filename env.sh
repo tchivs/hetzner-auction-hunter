0a63c8aedbf4f2a6db4c8192d8593f9e607af0f5
luckylinux <luckylinux>
2024-04-21 09:44:57 +0200

    Using shdotenv to load .env file into BASH.

--- env.sh
+++ env.sh
@@ -4,14 +4,9 @@
 relativepath="./" # Define relative path to go from this script to the root level of the tool
 if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
 
-# Load Settings and Credentials
-#for item in $(grep -v '^#' ${toolpath}/.env | xargs -d ' '); do v=$(eval $item); export v; done
-#for item in $(grep -v '^#' ${toolpath}/.env | xargs -d ' '); do export "$item"; done
-
-#source ${toolpath}/.env
-set -a
-source "${toolpath}/.env"
-set +a
+# Load the Environment Variables into THIS Script
+#shdoteven --env ".env"
+eval "$(shdotenv --env .env || echo \"exit $?\")"
 
 # Test that it worked
 echo "Host Data Path set to: ${APP_HOST_DATA_PATH}"
